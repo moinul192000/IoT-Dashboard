331b9f72700db2a90d3aae16f6a5741d8e3c51e6
Moinul Haq <moinul192000@gmail.com>
2020-06-29 12:03:03 +0800

    Primary Dashboard with Working API\n Created Record Class to store each record

---
+++ app/Http/Controllers/RecordController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Record;
+use Illuminate\Http\Request;
+
+class RecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Records = Record::all();
+        return view('Welcome',['Records' => $Records]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Record::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Record $record)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Record $record)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Record $record)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Record $record)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_06_28_070802_create_records_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('device')->nullable($value = true);
+            $table->float('ph', 4, 2);
+            $table->float('tempC', 4, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
